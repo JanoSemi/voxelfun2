01943a2ba30f4ec75c76b3bc460480e89e2bc47c
Janosch Lion <janorico@posteo.de>
2024-09-07 20:26:11 +0200

    Add voxelfun2 module

---
+++ modules/voxelfun2/register_types.cpp
@@ -0,0 +1,12 @@
+/* register_types.cpp */
+
+#include "register_types.h"
+
+#include "core/class_db.h"
+
+void register_voxelfun2_types() {
+}
+
+void unregister_voxelfun2_types() {
+   // Nothing to do here in this example.
+}
---
+++ modules/voxelfun2/register_types.h
@@ -0,0 +1,5 @@
+/* register_types.h */
+
+void register_voxelfun2_types();
+void unregister_voxelfun2_types();
+/* yes, the word in the middle must be the same as the module folder name */
